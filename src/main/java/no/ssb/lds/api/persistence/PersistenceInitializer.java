4eaf798d0ae827b3c0ee257ced355e260e81cf04
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-10-27 14:53:36 +0100

    Added specification to persistence-initializer.

--- src/main/java/no/ssb/lds/api/persistence/PersistenceInitializer.java
+++ src/main/java/no/ssb/lds/api/persistence/PersistenceInitializer.java
@@ -1,6 +1,7 @@
 package no.ssb.lds.api.persistence;
 
 import no.ssb.lds.api.persistence.reactivex.RxJsonPersistence;
+import no.ssb.lds.api.specification.Specification;
 
 import java.util.Map;
 import java.util.Set;
@@ -11,5 +12,5 @@ public interface PersistenceInitializer {
 
     Set<String> configurationKeys();
 
-    RxJsonPersistence initialize(String defaultNamespace, Map<String, String> configuration, Set<String> managedDomains);
+    RxJsonPersistence initialize(String defaultNamespace, Map<String, String> configuration, Set<String> managedDomains, Specification specification);
 }
